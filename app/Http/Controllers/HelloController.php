01245a1cc96eae26bf2b58b2bf5e93a9acb7eaa1
ks24pon <pom123re0624@gmail.com>
2021-02-23 10:38:05 +0900

    Laravel入門

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+
+    public function __invoke() {
+        return <<<EOF
+
+        <html>
+        <head>
+        <title>Hello</title>
+        <style>
+        body { font-size:16pt; color:#999; }
+      h1 { font-size:30pt; text-align:right; color:#eee;
+        margin:-15px 0px 0px 0px; }
+        </style>
+        </head>
+        <body>
+            <h1>Single Action</h1>
+            <p>これは、シングルアクションコントローラのアクションです。</p>
+        </body>
+        </html>
+    EOF;
+       }
+    }
+
+ 
+  
